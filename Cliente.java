3f50deab314fbf0e8cf05bd1b8c15183e71c3be4
Roberto_F.rocha <115853601+Roberto-F-Rocha@users.noreply.github.com>
2024-09-18 17:14:51 -0300

    Add files via upload
    Criação: Classe Cliente, construtor e métodos

---
+++ Cliente.java
@@ -0,0 +1,41 @@
+public class Cliente{
+
+    private int idCliente;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+    private int Controle;
+
+    public Cliente(int idCliente, String nome, String email, String telefone, String cpf, int Controle){
+            this.idCliente = idCliente;
+            this.nome = nome;
+            this.email = email;
+            this.telefone = telefone;
+            this.cpf = cpf;
+            this.Controle = Controle;
+    }
+
+    public void criarCliente(){
+
+    }
+
+    public void editarCliente(){
+
+    }
+
+    public void excluirCliente(){
+
+    }
+
+    public void visualizarCliente(){
+
+    }
+
+    public void listarCliente(){
+        
+    }
+
+
+
+}
