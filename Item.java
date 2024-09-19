94f9d85e44f7d27a6ef887f3088ad5216b169cea
angellusj <angela.souza@alunos.ufersa.edu.br>
2024-09-18 23:55:21 -0300

    criando classe item

---
+++ Item.java
@@ -0,0 +1,9 @@
+class Item {
+    private int quantidadeItem;
+    private float valorTotal;
+
+    public Item(int quantidadeItem, float valorTotal){
+        this.quantidadeItem = quantidadeItem;
+        this.valorTotal = valorTotal;
+    } 
+}
