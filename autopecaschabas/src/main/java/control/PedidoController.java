3c1b893b00f8235f03d5b962b8e09aaa1a48c34d
angellusj <angela.souza@alunos.ufersa.edu.br>
2024-10-08 20:42:35 -0300

    adicionando as tratativas no controller de pedido

--- autopecaschabas/src/main/java/control/PedidoController.java
+++ autopecaschabas/src/main/java/control/PedidoController.java
@@ -10,6 +10,31 @@ import model.dao.PedidoDAO;
 
 public class PedidoController {
     public static Pedido criaPedido(int idPedido, Date data, String status, Funcionario funcionario, Cliente cliente){
+        if (data == null) {
+            throw new IllegalArgumentException("Data não pode ser nulo.\n");
+        }
+
+        Date dataAtual = new Date();
+        if (data.after(dataAtual)) {
+            throw new IllegalArgumentException("Data inválida.\n");
+        }
+
+        if (status == null) {
+            throw new IllegalArgumentException("Status do pedido não pode ser nulo.\n");
+        }
+
+        if (!status.equals(Pedido.andamento) && !status.equals(Pedido.completo) && !status.equals(Pedido.problema) && !status.equals(Pedido.processando)){
+            throw new IllegalArgumentException("Status de pedido inválido.");
+        }
+
+        if (funcionario == null) {
+            throw new IllegalArgumentException("Funcionário não pode ser nulo.\n");
+        }
+
+        if (cliente == null) {
+            throw new IllegalArgumentException("Cliente não pode ser nulo.\n");
+        }
+
         Pedido pedido = new Pedido(idPedido, data, status, funcionario, cliente);
         PedidoDAO.criaPedido(pedido);
         return pedido;
