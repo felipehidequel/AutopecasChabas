6c35d741289a56bbd75e4bff96f9d4307eb9ef8d
angellusj <angela.souza@alunos.ufersa.edu.br>
2024-10-08 15:37:56 -0300

    Modificando controller de Cliente

--- autopecaschabas/src/main/java/control/ClienteController.java
+++ autopecaschabas/src/main/java/control/ClienteController.java
@@ -7,6 +7,48 @@ import java.util.List;
 
 public class ClienteController {
     public static Cliente cadastrarCliente(String nome, String telefone, String cpf) {
+        if (nome == null) {
+            throw new IllegalArgumentException("Nome não pode ser nulo.\n");
+        }
+        nome = nome.toUpperCase();
+
+        if (nome.trim().isEmpty()){
+            throw new IllegalArgumentException("Nome não pode ser vazio");
+        }
+        nome = nome.toUpperCase();
+
+        if (telefone == null) {
+            throw new IllegalArgumentException("Telefone não pode ser nulo.\n");
+        }
+
+        if (telefone.trim().isEmpty()){
+            throw new IllegalArgumentException("Telefone não pode ser vazio.\n");
+        }
+
+        if (telefone.matches("\\d+")){
+            throw new IllegalArgumentException("Telefone não pode conter letras.\n");
+        }
+
+        if (telefone.length() != 11){
+            throw new IllegalArgumentException("Digite apenas 11 números.");
+        }
+
+        if (cpf == null) {
+            throw new IllegalArgumentException("CPF não pode ser nulo.\n");
+        }
+
+        if (cpf.trim().isEmpty()){
+            throw new IllegalArgumentException("CPF não pode ser vazio.\n");
+        }
+
+        if (cpf.length() != 11){
+            throw new IllegalArgumentException("Digite apenas 11 números.\n");
+        }
+
+        if (cpf.matches("\\d+")){
+            throw new IllegalArgumentException("CPF não pode conter letras.");
+        }
+
         Cliente c = new Cliente(nome, telefone, cpf);
         ClienteDAO.criarCliente(c);
         return c;
