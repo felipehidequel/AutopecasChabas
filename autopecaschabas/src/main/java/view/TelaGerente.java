8ed4ad6bac8dff8c577f0602a6c8584de2c827f6
angellusj <angela.souza@alunos.ufersa.edu.br>
2024-10-11 16:02:40 -0300

    Adicionando atualização de estoque no menu do gerente

--- autopecaschabas/src/main/java/view/TelaGerente.java
+++ autopecaschabas/src/main/java/view/TelaGerente.java
@@ -1,7 +1,9 @@
 package view;
 
 import control.FuncionarioController;
+import control.PecaController;
 import model.entity.Funcionario;
+import model.entity.Peca;
 import utils.Logg;
 
 import java.util.List;
@@ -13,7 +15,7 @@ public class TelaGerente {
         Funcionario func = null;
         String nome, login, senha;
         boolean sair = false, gerente, funcionarioCadastrado = false;
-        int id;
+        int id, quantidade;
 
         try(Scanner sc = new Scanner(System.in)) {
             do {
@@ -21,7 +23,8 @@ public class TelaGerente {
                 Logg.info("1. Cadastrar Novo Funcionário");
                 Logg.info("2. Listar Funcionários");
                 Logg.info("3. Remover Funcionário");
-                Logg.info("4. Sair");
+                Logg.info("4. Atualizar estoque");
+                Logg.info("5. Sair");
 
                 int opcao = sc.nextInt();
                 sc.nextLine();
@@ -80,6 +83,22 @@ public class TelaGerente {
                         }
                         break;
                     case 4:
+                        Logg.info("Digite o ID do pedido que deseja atualizar:");
+                        id = sc.nextInt();
+                        sc.nextLine();
+
+                        Logg.info("Informe a quantidade que será adicionada ao estoque:");
+                        quantidade = sc.nextInt();
+                        sc.nextLine();
+
+                        try {
+                            PecaController.atualizarEstoqueByIdPeca(quantidade, id);
+                            Logg.info("Estoque da peça de ID " + id + " atualizado com sucesso!");
+                        } catch (IllegalArgumentException e) {
+                            Logg.warning("Erro ao atualizar estoque: " + e.getMessage());
+                        }
+                        break;
+                    case 5:
                         Logg.info("Saindo do menu do gerente.");
                         sair = true;
                         break;
